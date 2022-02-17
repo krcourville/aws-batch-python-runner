7a023341eb12846fcf35636ae6c611657732c582
Ken Courville <krcourville@users.noreply.github.com>
2022-02-17 09:00:24 -0700

    add egress

--- infrastructure/batch.tf
+++ infrastructure/batch.tf
@@ -30,6 +30,14 @@ resource "aws_iam_role_policy_attachment" "runner_batch_svc_role_attach_batch_se
 resource "aws_security_group" "runner_batch_compute_sg" {
   name   = "${local.prefix}-runner-batch-compute-sg"
   vpc_id = module.vpc.vpc_id
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 resource "aws_batch_compute_environment" "runner_batch_compute_env" {
